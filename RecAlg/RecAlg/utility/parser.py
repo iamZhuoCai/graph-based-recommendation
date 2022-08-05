aa157b8dd1a486fc20896a7847b087d33e1088ff
czhuo <48952842+iamZhuoCai@users.noreply.github.com>
2022-08-05 11:07:26 +0800

    Update parser.py

--- RecAlg/RecAlg/utility/parser.py
+++ RecAlg/RecAlg/utility/parser.py
@@ -1,10 +1,3 @@
-'''
-Created on Oct 10, 2018
-Tensorflow Implementation of Neural Graph Collaborative Filtering (NGCF) model in:
-Wang Xiang et al. Neural Graph Collaborative Filtering. In SIGIR 2019.
-
-@author: Xiang Wang (xiangwang@u.nus.edu)
-'''
 import argparse
 
 def parse_args():
